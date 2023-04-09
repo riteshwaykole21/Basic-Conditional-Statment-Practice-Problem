c5dff1a64886622871db9089bcfacbdf20c4e16d
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-09 21:56:54 +0530

    [Ritesh]add :Check Both Number Are Equal Or Not

---
+++ Conditions/Equals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions
+{
+    public class Equals
+    {
+        public void Integer()
+        {
+            Console.WriteLine("Enter The First Number");
+            int FirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+            if(FirstNumber == SecondNumber)
+            {
+                Console.WriteLine("{0} && {0} Both Number Are Equal" , FirstNumber,SecondNumber);
+            }
+            else
+            {
+                Console.WriteLine("The Number Are Not Equal");
+            }
+        }
+    }
+}
---
+++ Conditions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Check The Equal Or Not");
+            Console.WriteLine("Emnter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    Equals equals = new Equals();
+                    equals.Integer();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
